920c9f29eef7745202f95359ca8f28110578b781
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2022-06-03 23:21:43 +0600

    Create App.java

---
+++ Problems/First/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Results results = new Results();
+
+        results.setRoll(100);
+        results.setMarks(22.0F, 10.0F);
+        results.display();
+    }
+}
