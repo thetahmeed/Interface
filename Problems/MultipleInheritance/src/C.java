941bf69776822db7efc6fca154dfc5dc17371c0d
Tahmeedul Islam <70460495+thetahmeed@users.noreply.github.com>
2022-06-22 20:37:38 +0600

    Create C.java

---
+++ Problems/MultipleInheritance/src/C.java
@@ -0,0 +1,10 @@
+public class C implements A, B{
+
+    public void dog() {
+        System.out.println("I am dog");
+    }
+    public void cat() {
+        System.out.println("I am cat");
+    }
+    
+}
